84069789f141c164c1ff2e1ba22ec8a71c7a7cfc
Birhanu <birhanuasmamaw24@gmail.com>
2024-01-12 06:36:19 -1000

    Editing the layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className='w-full sticky relative'>
-
         <NavBar/>
         </div>
         {children}
