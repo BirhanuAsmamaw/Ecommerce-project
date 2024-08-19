3faeeb5541e64756585c901c827e4be56e18f249
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-08-19 07:39:58 -0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from './components/NavBar'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
