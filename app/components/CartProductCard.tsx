89fd582d09446802d887f701036d834c2f0f7827
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-08-28 10:45:42 -0900

    Update CartProductCard.tsx

--- app/components/CartProductCard.tsx
+++ app/components/CartProductCard.tsx
@@ -12,6 +12,7 @@ const CartProduct = ({ image, name, price, inCart }: productProp) => {
     const handleRemoveFromCart = () => {
         removeFromCart(name);
       };
+    
     return ( 
         <li className="flex py-6">
         <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
